1a71b07d3cb1ffa49d49910b085b5fb022042cfc
song <1054979645@qq.com>
2021-11-05 11:30:43 +0800

    增加一个新的分支用来存放代码

---
+++ 代码学习.py
@@ -0,0 +1,15 @@
+config['bpr_batch_size'] = args.bpr_batch
+config['latent_dim_rec'] = args.recdim
+config['lightGCN_n_layers'] = args.layer
+config['dropout'] = args.dropout
+config['keep_prob'] = args.keepprob
+config['A_n_fold'] = args.a_fold
+config['test_u_batch_size'] = args.testbatch
+config['multicore'] = args.multicore
+config['lr'] = args.lr
+config['decay'] = args.decay
+config['pretrain'] = args.pretrain
+config['replace_ratio'] = args.replace_ratio
+config['similarity_ratio'] = args.similarity_ratio
+config['A_split'] = False
+config['bigdata'] = False
